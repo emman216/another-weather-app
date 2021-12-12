18eeb8c92833565be7c2c93394e22c64d52d92a2
emman216 <pagcaliwaganemman@gmail.com>
2021-12-12 21:06:28 +0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for Express Config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -67,12 +68,6 @@ app.get("/weather", (req, res) => {
       });
     }
   );
-
-  // res.send({
-  //   forecast: "It is snowing",
-  //   location: "Philadelphia",
-  //   address: req.query.address,
-  // });
 });
 
 app.get("/products", (req, res) => {
@@ -104,6 +99,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
